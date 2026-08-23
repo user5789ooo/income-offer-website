f870067d8bc4bb8d6688dfd21fb6a0cc9a0055a9
Income Offers <taniamollick453@gmail.com>
2026-08-24 04:29:06 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,19 @@
-const SUPABASE_URL = "https://ylquvyebfgsvipecljuu.supabase.co";
+const SUPABASE_URL =
+  "https://ylquvyebfgsvipecljuu.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_Ur8pHDUGsj8yTqo4cnEdmA_hoLwHYxT";
+const SUPABASE_ANON_KEY =
+  "sb_publishable_Ur8pHDUGsj8yTqo4cnEdmA_hoLwHYxT";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true,
+        storage: window.localStorage
+      }
+    }
+  );
